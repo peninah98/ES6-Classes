890088febc2abca5c12c369461fd9a61122312a3
peninah98 <peninangizwena@gmail.com>
2023-07-28 18:52:08 +0200

    Classes

---
+++ operators/restOperator.js
@@ -0,0 +1,10 @@
+"use strict"
+function printUser(firstname,lastname,email,...question){
+    
+    return `Hello my name is ${firstname} ${lastname} from Rwanda here is my ${email} fell free to reach out to me if you have any ${question}`
+
+
+}
+
+console.log(printUser('Nora','Barbie','barbienorie@gmail.com','question','to ask .'))
+
