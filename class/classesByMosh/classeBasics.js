bdf3452b0822f56fc4cf8d714c41d57ffcfa1e21
peninah98 <peninangizwenayo@gmail.com>
2023-11-30 15:25:12 +0200

    feat: creating the factories methods in the obkect

--- class/classesByMosh/classeBasics.js
+++ class/classesByMosh/classeBasics.js
@@ -1,3 +1,4 @@
+// This is object literal synthax
 const cirle = {
     radius: 1,
     location: {
@@ -10,3 +11,24 @@ const cirle = {
 }
 
 cirle.draw()
+
+
+const newCirle =JSON.parse(JSON.stringify(cirle))
+console.log(cirle)
+console.log(newCirle)
+
+
+// Factories
+function CreateCircle(radius){
+    return{
+        radius,
+        draw: function(){
+            console.log("draw")
+        }
+    }
+}
+
+const circle = CreateCircle(1)
+circle.draw()
+
+// factories is the way of creating the method in the object
