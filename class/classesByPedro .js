659656c87f957d77032977066ef7fde2061377ee
peninah98 <peninangizwenayo@gmail.com>
2023-11-30 16:28:39 +0200

    feat: creating second person

--- class/classesByPedro .js
+++ class/classesByPedro .js
@@ -3,8 +3,15 @@ class Person {
         this.name = name
         this.age = age
     }
+    getName =()=>{
+        return this.name
+    }
+    getAge =()=> this.age
+
 }
 
-let person = new Person("Pedro", 27)
-console.log(person.name)
+let person1 = new Person("Clara", 27)
+let person2= new Person("Pedro", 12)
+console.log(person1.getName())
+console.log(person2.getName())
 
