3ca13e76736b667f35a2602f5181f58e021b63dd
peninah98 <peninangizwenayo@gmail.com>
2023-11-30 15:36:16 +0200

    feat: learning the oop at any cost

--- class/classesByMosh/classeBasics.js
+++ class/classesByMosh/classeBasics.js
@@ -13,16 +13,16 @@ const cirle = {
 cirle.draw()
 
 
-const newCirle =JSON.parse(JSON.stringify(cirle))
+const newCirle = JSON.parse(JSON.stringify(cirle))
 console.log(cirle)
 console.log(newCirle)
 
 
 // Factories
-function CreateCircle(radius){
-    return{
+function CreateCircle(radius) {
+    return {
         radius,
-        draw: function(){
+        draw: function () {
             console.log("draw")
         }
     }
@@ -32,3 +32,32 @@ const circle = CreateCircle(1)
 circle.draw()
 
 // factories is the way of creating the method in the object
+// factory functions return the  new object when you call the function
+
+
+function createCar(brand) {
+    return {
+        brand,
+        drive: function () {
+            console.log(`${this.brand} is the very fast car!`)
+        }
+    }
+}
+
+const myChoice = createCar("BMW")
+myChoice.drive()
+
+
+
+
+function createZoo(animal){
+    return{
+        animal,
+        feed: function(){
+            console.log(`Is technologies food? many of them are ${this.animal}`)
+        }
+    }
+}
+
+const github = createZoo("cat")
+github.feed()
