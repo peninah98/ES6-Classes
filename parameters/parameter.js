255b413c2d5840c9288e1b6eb3661a7faf7b2bf6
peninah98 <peninangizwenayo@gmail.com>
2024-02-08 12:09:40 +0200

    feat: creating the function with parameter and arguments

---
+++ parameters/parameter.js
@@ -0,0 +1,7 @@
+// In this situation name is the parameter passed to the function during the declaration 
+function displayName(name){
+    return name
+}
+// Penina is the argumento of this function as it is passed during the function call
+// The agruments are the actual values function take
+console.log(displayName("Penina"))
