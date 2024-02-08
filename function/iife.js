2de70114db2618fba0d45308bbd72a0494919f23
peninah98 <peninangizwenayo@gmail.com>
2024-02-08 17:56:40 +0200

    feat: creatint the function

--- function/iife.js
+++ function/iife.js
@@ -1,4 +1,10 @@
 
-(function (message) {
-  console.log(message)
-})("Hello, IIFE!")
+// ;(function (message) {
+//   console.log(message)
+// })("Hello, IIFE!")
+
+
+(function (a, b) {
+  let sum = a - b
+  console.log(sum);
+})(2, 4)
