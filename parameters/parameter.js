6f4c82647bf010d053871ca5cfc773b64d85c583
peninah98 <peninangizwenayo@gmail.com>
2024-02-08 12:21:47 +0200

    feat: adding the arguments inthe functionN

--- parameters/parameter.js
+++ parameters/parameter.js
@@ -1,7 +1,10 @@
 // In this situation name is the parameter passed to the function during the declaration 
-function displayName(name){
-    return name
+function displayName(name, name2, name3){
+    return [name, name2, name3]
 }
 // Penina is the argumento of this function as it is passed during the function call
 // The agruments are the actual values function take
-console.log(displayName("Penina"))
+console.log(displayName("Penina", "Agnes", "Yves"))
+
+console.log(arguments[0]);
+
