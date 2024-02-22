1540ff68ebd821d84da922866d5c7784f7df7e2b
peninah98 <peninangizwenayo@gmail.com>
2024-02-22 17:05:28 +0200

    feat:iterating in the objects using values method

---
+++ Object/toArray.js
@@ -0,0 +1,9 @@
+const person = {
+  name: "Agnes",
+  hobby: "Reading",
+}
+// for (let key in person) {
+//   console.log(person[key])
+// }
+
+console.log(Object.values(person).forEach((keys) => console.log(keys)))
