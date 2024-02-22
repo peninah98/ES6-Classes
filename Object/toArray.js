4b3bd0d52f851aafa54d2c9f979146210c309e8c
peninah98 <peninangizwenayo@gmail.com>
2024-02-22 17:16:23 +0200

    feat: create the iteratio

--- Object/toArray.js
+++ Object/toArray.js
@@ -1,9 +1,14 @@
 const person = {
   name: "Agnes",
   hobby: "Reading",
+  skinColor:"Reading"
 }
-// for (let key in person) {
-//   console.log(person[key])
-// }
+for (let key in person) {
+    if(person[key] === "Reading"){
+        console.log(`${key}: ${person[key]}`)
+    }else{
+        continue;
+    }
+}
 
-console.log(Object.values(person).forEach((keys) => console.log(keys)))
+// console.log(Object.values(person).forEach((keys) => console.log(keys)))
