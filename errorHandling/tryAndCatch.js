c0d9f963fdfae77b758328afd6ce1f7b2ae2108f
peninah98 <peninangizwenayo@gmail.com>
2024-02-23 10:45:07 +0200

    feat:error handling

--- errorHandling/tryAndCatch.js
+++ errorHandling/tryAndCatch.js
@@ -1,2 +1,6 @@
-const result = someUnidefiendVariable + 79
-console.log(result);
+try {
+  const result = someUnidefiendVariable + 79
+  console.log(result)
+} catch (error) {
+  console.log("Error occured ", error.message)
+}
