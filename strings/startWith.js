d604f358b91d1ba2f8e315f452dc494ade8c5f19
Penine  Ngizwenayo <peninangizwenayo@gmail.com>
2024-05-20 08:53:20 +0200

    feat: create string method ends with which returns boolean

--- strings/startWith.js
+++ strings/startWith.js
@@ -2,3 +2,5 @@ let sentence = "Hello, there"
 console.log(sentence.startsWith("Hello"))
 
 console.log(sentence.includes("p"))
+console.log(sentence.endsWith("e"))
+console.log(sentence.endsWith("o"))
