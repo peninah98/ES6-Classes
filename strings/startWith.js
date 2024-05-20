cb48afb13e3144ccdfb8ac6b0212709d535e6c7c
Penine  Ngizwenayo <peninangizwenayo@gmail.com>
2024-05-20 19:08:01 +0200

    feat:string method

--- strings/startWith.js
+++ strings/startWith.js
@@ -1,6 +1,13 @@
-let sentence = "Hello, there"
+let sentence = "Hello, there "
 console.log(sentence.startsWith("Hello"))
 
 console.log(sentence.includes("p"))
 console.log(sentence.endsWith("e"))
 console.log(sentence.endsWith("o"))
+console.log(sentence.repeat(3))
+
+const str = "\u004F\u030C"
+
+console.log(str.normalize())
+
+console.log(String.fromCodePoint(65, 66, 67))
