70b0884ffa2750c0155e88e862bcac19e347c483
Penine  Ngizwenayo <peninangizwenayo@gmail.com>
2024-05-21 21:39:36 +0200

    feat: separating numbers and  strings

---
+++ challenges/sortNumbers.js
@@ -0,0 +1,11 @@
+const arr = ["hello12", "pe34", "ooops98"]
+const result = []
+const numbers = []
+const finalResult = arr.map((element) => {
+  element.split(" ").filter((item) => {
+    if (typeof item === "string") result.push(item).join("")
+    return numbers.push(item).join()
+  })
+})
+
+console.log(finalResult)
