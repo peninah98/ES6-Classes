09c7700f5c3ba344ddc768a7d9c1bbb586784458
Peninah98 <peninangizwenayo@gmail.com>
2025-03-03 17:33:44 +0200

    feat: create object literals

--- class/car.js
+++ class/car.js
@@ -1,9 +1,10 @@
 const book ={
     title:"Hunger Games 1",
-    author:"Harper Lee",
+    author:"Suzanne Collins",
     publicationYear:1998,
     getSummary: function(){
         return `${this.title} was written by ${this.author} and is published by ${this.publicationYear}`
 }
     }
 console.log(book.getSummary());
+console.log(Object.keys(book))
