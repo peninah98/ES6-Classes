e6c61511da9434de438cf8e77faf6b0d72c9d884
Peninah98 <peninangizwenayo@gmail.com>
2025-03-03 17:30:49 +0200

    feat: create object literals

--- class/car.js
+++ class/car.js
@@ -1,5 +1,9 @@
-class Car{
-    consrtuctor Car(){
-        
-    }
+const book ={
+    title:"Hunger Games 1",
+    author:"Harper Lee",
+    publicationYear:1998,
+    getSummary: function(){
+        return `${this.title} was written by ${this.author} and is published by ${this.publicationYear}`
 }
+    }
+console.log(book.getSummary());
